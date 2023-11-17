72ce18a83fc47549fe9071d052d02c5f84d1eca3
Nahuel LADER <nahuelchierichetti@gmail.com>
2023-11-17 08:32:08 -0300

    Ajustes de diseño

--- src/data/asyncMock.jsx
+++ src/data/asyncMock.jsx
@@ -38,6 +38,54 @@ const usuarios = [
         empresa: 'X',
         table: 8,
         points: 28
+    },
+    {
+        id: 6,
+        name: 'Martin',
+        last_3_dni: '756',
+        empresa: 'Pedidos Ya',
+        table: 5,
+        points: 23
+    },
+    {
+        id: 7,
+        name: 'Lucía',
+        last_3_dni: '896',
+        empresa: 'Google',
+        table: 6,
+        points: 5
+    },
+    {
+        id: 8,
+        name: 'Santiago',
+        last_3_dni: '063',
+        empresa: 'Facebook',
+        table: 6,
+        points: 75
+    },
+    {
+        id: 9,
+        name: 'Milagros',
+        last_3_dni: '432',
+        empresa: 'Globant',
+        table: 6,
+        points: 56
+    },
+    {
+        id: 10,
+        name: 'Martina',
+        last_3_dni: '675',
+        empresa: 'Microsoft',
+        table: 6,
+        points: 63
+    },
+    {
+        id: 11,
+        name: 'Agustina',
+        last_3_dni: '243',
+        empresa: 'Adventure',
+        table: 6,
+        points: 49
     }
 ];
 
--- src/views/Board.jsx
+++ src/views/Board.jsx
@@ -2,9 +2,10 @@
 import { useEffect, useState } from 'react';
 import { getUsuarios } from '../data/asyncMock';
 import ItemList from '../components/ItemList/ItemList';
-import imgGlobos from '../assets/img/globos.png';
-import imgLogoWarner from '../assets/img/warner-logo.png';
-import titleJerrys from '../assets/img/titulo-jerry.png'
+import ImgGlobos from '../assets/img/globos.png';
+import ImgLogoWarner from '../assets/img/warner-logo.png';
+//import titleJerrys from '../assets/img/titulo-jerry.png'
+import TitleDashboard from '../components/TitleDashboard';
 import './Board.css'
 
 
@@ -20,12 +21,12 @@ const Board = () => {
   return (
     <div className='bg-black text-yellow board'>
       <div className='p-[4em]'>
-        <h2 className='py-[30px] black fontSize-[40px] text-center'>DASHBOARD JERRY´S23</h2>
+        <TitleDashboard />
         <ItemList data={data} />
       </div>
       <div className='img-board'>
-        <img src={imgGlobos} alt="" className='imgGlobos'/>
-        <img src={imgLogoWarner} alt="" className='imgWarner'/>
+        <img src={ImgGlobos} alt="" className='imgGlobos'/>
+        <img src={ImgLogoWarner} alt="" className='imgWarner'/>
       </div>
     </div>
   );
